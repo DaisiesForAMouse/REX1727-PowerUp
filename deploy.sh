60ab53451c56df88fd0e50eb5ab358b6a863eed5
David Chen <david.chen9909@gmail.com>
2018-02-14 18:28:17 -0500

    Removed hanging ping flags

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,11 @@
 
 echo "deploy.sh: Starting deploy process..."
 
+cur=`pwd`
+
+dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $dir
+
 team=1727
 
 # Only works for four digit teams
@@ -13,37 +18,39 @@ if [ ! -f "FRCUserProgram" ]; then
 fi
 
 echo "First trying to connect on mDNS..."
-ping -n 4 "roboRIO-$team-FRC.local" > /dev/null 2>&1 ; mDNS=$?
+ping -c 1 "roboRIO-$team-FRC.local" > /dev/null 2>&1 ; mDNS=$?
 if [ $mDNS -ne 0 ]; then
     echo "roboRIO not found on mDNS at roboRIO-$team-FRC.local, falling back to static USB"
-    ping -n 4 "172.22.11.2" > /dev/null 2>&1 ; static=$?
+    ping -c 1 "172.22.11.2" > /dev/null 2>&1 ; static=$?
     if [ $static -ne 0 ] ; then
         echo "Failed to connect via static USB, falling back to static ethernet..."
-        ping -n 4 "10.$team_ip.2" > /dev/null 2>&1 ; ethernet=$?
+        ping -c 1 "10.$team_ip.2" > /dev/null 2>&1 ; ethernet=$?
         if [ $ethernet -ne 0 ] ; then
             echo "Connection failed on 10.$team_ip.2."
             echo "Connection to roboRIO failed."
             echo "deploy.sh: Exiting deploy process..."
             exit 1
         else
-            ssh "lvuser@10.$team_ip.2" "killall -9 FRCUserProgram && rm ~/FRCUserProgram && exit"
+            ssh "lvuser@10.$team_ip.2" "killall -9 FRCUserProgram && rm ~/FRCUserProgram && exit" > /dev/null 2>&1
 
             scp "FRCUserProgram" "lvuser@10.$team_ip.2:~/FRCUserProgram"
 
             echo "Deployed via static ethernet."
         fi
     else
-	ssh "lvuser@172.22.11.2" "killall -9 FRCUserProgram && rm ~/FRCUserProgram && exit"
+    ssh "lvuser@172.22.11.2" "killall -9 FRCUserProgram && rm ~/FRCUserProgram && exit" > /dev/null 2>&1
 
-	scp "FRCUserProgram" "lvuser@172.22.11.2:~/FRCUserProgram"
+    scp "FRCUserProgram" "lvuser@172.22.11.2:~/FRCUserProgram"
         echo "Deployed via static USB."
     fi
 else
-    ssh "lvuser@roboRIO-$team-FRC.local" "killall -9 FRCUserProgram && rm ~/FRCUserProgram && exit"
+    ssh "lvuser@roboRIO-$team-FRC.local" "killall -9 FRCUserProgram && rm ~/FRCUserProgram && exit" > /dev/null 2>&1
 
     scp "FRCUserProgram" "lvuser@roboRIO-$team-FRC.local:~/FRCUserProgram"
     echo "Deployed via mDNS."
 fi
+
+cd $cur
 
 echo "deploy.sh: Exiting deploy process..."
 exit
