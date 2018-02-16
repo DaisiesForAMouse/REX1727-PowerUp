5b1e7008a719911be5f93c919527594e9a00dd36
David Chen <david.chen9909@gmail.com>
2018-02-16 10:37:10 -0500

    Added timeout of 1.5 seconds for each ping cmd

--- deploy.sh
+++ deploy.sh
@@ -18,13 +18,13 @@ if [ ! -f "FRCUserProgram" ]; then
 fi
 
 echo "First trying to connect on mDNS..."
-ping -c 1 "roboRIO-$team-FRC.local" > /dev/null 2>&1 ; mDNS=$?
+ping -c 1 -W 1.5 "roboRIO-$team-FRC.local" > /dev/null 2>&1 ; mDNS=$?
 if [ $mDNS -ne 0 ]; then
     echo "roboRIO not found on mDNS at roboRIO-$team-FRC.local, falling back to static USB"
-    ping -c 1 "172.22.11.2" > /dev/null 2>&1 ; static=$?
+    ping -c 1 -W 1.5 "172.22.11.2" > /dev/null 2>&1 ; static=$?
     if [ $static -ne 0 ] ; then
         echo "Failed to connect via static USB, falling back to static ethernet..."
-        ping -c 1 "10.$team_ip.2" > /dev/null 2>&1 ; ethernet=$?
+        ping -c 1 -W 1.5 "10.$team_ip.2" > /dev/null 2>&1 ; ethernet=$?
         if [ $ethernet -ne 0 ] ; then
             echo "Connection failed on 10.$team_ip.2."
             echo "Connection to roboRIO failed."
